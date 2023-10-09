9448d82fd1ee82e4571db285a045788b9ca97abe
joaopedromateus <joaopmdas@gmail.com>
2023-10-08 22:14:54 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,120 @@
+
+//1
+document.getElementById('mostrarAlerta').addEventListener('click', function() {
+    alert("Hello World!");
+});
+
+
+
+//2
+// Declare duas variáveis
+var numero1 = 5;
+var numero2 = 7;
+
+// Calcule a soma
+var resultado = numero1 + numero2;
+
+// Exiba o resultado no console do navegador
+console.log("A soma de " + numero1 + " e " + numero2 + " é igual a " + resultado);
+
+
+
+
+//3
+// Declare uma variável com um valor
+var minhaVariavel = 42;
+
+// Verifique se a variável é um número
+if (typeof minhaVariavel === 'number') {
+    console.log("É um número");
+} else {
+    console.log("Não é um número");
+}
+
+
+
+//4
+// Declare uma variável com um valor
+var minhaVariavel = "Isso é uma string";
+
+// Verifique se a variável é uma string
+if (typeof minhaVariavel === 'string') {
+    console.log("É uma string");
+} else {
+    console.log("Não é uma string");
+}
+
+
+//5
+// Declare uma variável com um valor booleano
+var minhaVariavel = true;
+
+// Verifique se a variável é um booleano
+if (typeof minhaVariavel === 'boolean') {
+    console.log("É um booleano");
+} else {
+    console.log("Não é um booleano");
+}
+
+
+//6
+// Declare duas variáveis
+var numero1 = 10;
+var numero2 = 5;
+
+// Calcule a subtração
+var resultado = numero1 - numero2;
+
+// Exiba o resultado
+console.log("A subtração de " + numero1 + " e " + numero2 + " é igual a " + resultado);
+
+
+//7
+// Declare duas variáveis
+var numero1 = 7;
+var numero2 = 3;
+
+// Calcule a multiplicação
+var resultado = numero1 * numero2;
+
+// Exiba o resultado
+console.log("A multiplicação de " + numero1 + " e " + numero2 + " é igual a " + resultado);
+
+
+//8
+// Declare duas variáveis
+var numero1 = 15;
+var numero2 = 3;
+
+// Calcule a divisão
+var resultado = numero1 / numero2;
+
+// Exiba o resultado
+console.log("A divisão de " + numero1 + " por " + numero2 + " é igual a " + resultado);
+
+
+//9
+// Declare uma variável
+var numero = 8;
+
+// Verifique se é um número par
+if (numero % 2 === 0) {
+    console.log("É um número par");
+} else {
+    console.log("Não é um número par");
+}
+
+
+
+
+
+//10
+// Declare uma variável
+var numero = 7;
+
+// Verifique se é um número ímpar
+if (numero % 2 !== 0) {
+    console.log("É um número ímpar");
+} else {
+    console.log("Não é um número ímpar");
+}
